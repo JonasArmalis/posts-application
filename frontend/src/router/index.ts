69a4b74191d90935e1b2ea2d39814c9fe668cf90
Erikas Lauzadis <erikas.lauzadis@teltonika.lt>
2024-07-24 11:31:02 +0300

    Created all page views and routes to display them

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,8 @@
+import AuthorsPage from '@/components/views/AuthorsPage.vue'
+import PostDetailPage from '@/components/views/PostDetailPage.vue'
+import PostsPage from '@/components/views/PostsPage.vue'
+import UserLoginPage from '@/components/views/UserLoginPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +10,22 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: PostsPage
+    },
+    {
+      path: '/post-details',
+      name: 'post detail page',
+      component: PostDetailPage
+    },
+    {
+      path: '/authors',
+      name: 'author page',
+      component: AuthorsPage
+    },
+    {
+      path: '/login',
+      name: 'user login page',
+      component: UserLoginPage
     }
   ]
 })
