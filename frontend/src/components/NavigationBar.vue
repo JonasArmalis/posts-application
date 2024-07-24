8597f01a36915a23f65378795a4f2a504b1440a0
Erikas Lauzadis <erikas.lauzadis@teltonika.lt>
2024-07-24 11:51:56 +0300

    added a navigation bar component

---
+++ frontend/src/components/NavigationBar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <nav>
+            <RouterLink to="/">Posts</RouterLink>
+            <RouterLink to="/authors">Authors</RouterLink>
+            <RouterLink to="/login">Login</RouterLink>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: left;
+    margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+    color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+    background-color: transparent;
+}
+
+nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+    border: 0;
+}
+</style>
