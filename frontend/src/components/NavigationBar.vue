1cf3d2927c13d8649edc65218929fd7874b39463
Erikas Lauzadis <erikas.lauzadis@teltonika.lt>
2024-07-25 12:00:58 +0300

    added navbar logo and a favicon

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -3,7 +3,12 @@
 
 <template>
     <nav class="navbar is-link" role="navigation" aria-label="main navigation">
-        <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-brand">
+            <a class="navbar-item">
+                <img src="/logo.svg" alt="Logo">
+            </a>
+        </div>
+        <div class="navbar-menu">
             <div class="navbar-start">
                 <RouterLink class="navbar-item" to="/">Posts</RouterLink>
                 <RouterLink class="navbar-item" to="/authors">Authors</RouterLink>
