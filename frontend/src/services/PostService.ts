eeb1d83942eb7b34facd545593e33b1741f44130
Erikas Lauzadis <erikas.lauzadis@teltonika.lt>
2024-07-25 16:34:04 +0300

    added posts service and interface

---
+++ frontend/src/services/PostService.ts
@@ -0,0 +1,16 @@
+import httpClient from './HttpClient'
+import type { Post } from '../interfaces/Post'
+
+const END_POINT = '/posts'
+
+const getAllPosts = async (): Promise<Post[]> => {
+  const response = await httpClient.get<Post[]>(END_POINT)
+  return response.data
+}
+
+const getPost = async (id: number): Promise<Post> => {
+  const response = await httpClient.get<Post>(`${END_POINT}/${id}`)
+  return response.data
+}
+
+export { getAllPosts, getPost }
