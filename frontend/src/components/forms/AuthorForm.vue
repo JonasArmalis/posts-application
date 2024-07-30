3f21d7f12390973f01da6ed0c2b33c527ab3f989
Erikas Lauzadis <erikas.lauzadis@teltonika.lt>
2024-07-30 15:47:47 +0300

    allowed lithuanian letters

--- frontend/src/components/forms/AuthorForm.vue
+++ frontend/src/components/forms/AuthorForm.vue
@@ -10,13 +10,13 @@ const schema = yup.object({
         .required('Name is required')
         .min(2, 'Name must be at least 2 characters')
         .test('not-only-spaces', 'Name must consist of at least 2 non-space characters', value => value !== undefined && value.trim().length >= 2)
-        .matches(/^[a-zA-Z\s]*$/, 'Name cannot contain numbers or special symbols')
+        .matches(/^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ\s]*$/, 'Name cannot contain numbers or special symbols')
         .max(50, 'Name cannot be more than 50 characters'),
     surname: yup.string()
         .required('Surname is required')
         .min(2, 'Surname must be at least 2 characters')
         .test('not-only-spaces', 'Surname must consist of at least 2 non-space characters', value => value !== undefined && value.trim().length >= 2)
-        .matches(/^[a-zA-Z\s]*$/, 'Surname cannot contain numbers or special symbols')
+        .matches(/^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ\s]*$/, 'Name cannot contain numbers or special symbols')
         .max(50, 'Surname cannot be more than 50 characters')
 });
 
